81c4c2f2cae40e6a904f20b794a22a99c4921c15
Azhagusundaram.t <Azhagusundaram.t@zohocorp.com>
2021-05-20 20:24:43 +0530

    Initial Commit

---
+++ MatchedGeometry/MatchedGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchedGeometryApp.swift
+//  MatchedGeometry
+//
+//  Created by Azhagusundaram Tamil on 20/05/21.
+//
+
+import SwiftUI
+
+@main
+struct MatchedGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
